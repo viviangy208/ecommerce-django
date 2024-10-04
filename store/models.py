a0423eea48207526e2d77c4241ba3c328a8d1cc4
Vivian Uribe <viviangy208@gmail.com>
2024-10-04 17:38:47 +0200

    Actualizaciones 04102024

--- store/models.py
+++ store/models.py
@@ -19,4 +19,28 @@ class Product(models.Model):
         return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
-        return self.product_name 
+        return self.product_name
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+    
+    def tallas(self):
+        return super(VariationManager, self).filter(variation_category='talla', is_active=True)
+     
+variation_caterogy_choice = (
+    ('color', 'color'),
+    ('talla', 'talla'),
+)
+    
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_caterogy_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __unicode__(self):
+        return self.product
