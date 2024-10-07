92b6259e8d0d47ecc2479f6dace19a55a3a421ea
Vivian Uribe <viviangy208@gmail.com>
2024-10-07 15:01:22 +0200

    Cambios 07/10/2024

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -12,6 +12,21 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    product_variation = []
+
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+
+
+
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
@@ -22,6 +37,11 @@ def add_cart(request, product_id):
 
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
+
         cart_item.quantity +=1
         cart_item.save()
     except CartItem.DoesNotExist:
@@ -30,6 +50,10 @@ def add_cart(request, product_id):
             quantity = 1,
             cart = cart,
         )
+        if len(product_variation) > 0:
+            for item in product_variation:
+                cart_item.variations.add(item)
+
         cart_item.save()
     
     return redirect('cart')
@@ -57,6 +81,8 @@ def remove_cart_item(request, product_id):
 
 
 def cart(request, total=0, quantity=0, cart_items=None):
+    tax = 0
+    grand_total = 0
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
